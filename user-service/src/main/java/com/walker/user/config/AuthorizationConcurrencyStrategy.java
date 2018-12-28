01411d342fa33c9be7a9ec671d68044afca175ba
walker <qinmu911@163.com>
2018-12-28 18:02:40 +0800

    feat(hystrix): strategy

--- user-service/src/main/java/com/walker/user/config/AuthorizationConcurrencyStrategy.java
+++ user-service/src/main/java/com/walker/user/config/AuthorizationConcurrencyStrategy.java
@@ -1,9 +1,17 @@
 package com.walker.user.config;
 
+import com.netflix.hystrix.HystrixThreadPoolKey;
+import com.netflix.hystrix.HystrixThreadPoolProperties;
 import com.netflix.hystrix.strategy.concurrency.HystrixConcurrencyStrategy;
+import com.netflix.hystrix.strategy.concurrency.HystrixRequestVariable;
+import com.netflix.hystrix.strategy.concurrency.HystrixRequestVariableLifecycle;
+import com.netflix.hystrix.strategy.properties.HystrixProperty;
 import org.springframework.web.context.request.RequestContextHolder;
 
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author walker
@@ -11,9 +19,33 @@ import java.util.concurrent.Callable;
  */
 public class AuthorizationConcurrencyStrategy extends HystrixConcurrencyStrategy {
 
+    public AuthorizationConcurrencyStrategy() {
+        super();
+    }
+
     @Override
     public <T> Callable<T> wrapCallable(Callable<T> callable) {
         return new HystrixThreadCallable<>(RequestContextHolder.getRequestAttributes(), callable,
                 HystrixThreadLocal.threadLocal.get());
+    }
+
+    @Override
+    public ThreadPoolExecutor getThreadPool(HystrixThreadPoolKey threadPoolKey, HystrixProperty<Integer> corePoolSize, HystrixProperty<Integer> maximumPoolSize, HystrixProperty<Integer> keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
+        return super.getThreadPool(threadPoolKey, corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+    }
+
+    @Override
+    public ThreadPoolExecutor getThreadPool(HystrixThreadPoolKey threadPoolKey, HystrixThreadPoolProperties threadPoolProperties) {
+        return super.getThreadPool(threadPoolKey, threadPoolProperties);
+    }
+
+    @Override
+    public BlockingQueue<Runnable> getBlockingQueue(int maxQueueSize) {
+        return super.getBlockingQueue(maxQueueSize);
+    }
+
+    @Override
+    public <T> HystrixRequestVariable<T> getRequestVariable(HystrixRequestVariableLifecycle<T> rv) {
+        return super.getRequestVariable(rv);
     }
 }
